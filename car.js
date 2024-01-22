966f090ecb828daeb0eae0de3527cf06809b241c
Yavuz Yalçın <yyalcin41@gmail.com>
2024-01-22 11:27:17 +0300

    neural network and parallelization done.

--- car.js
+++ car.js
@@ -12,10 +12,14 @@ class Car {
         this.angle = 0;
         this.damaged = false;
 
+        this.useBrain = controlType == "AI";
+
         if (controlType != "DUMMY") {
             this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork(
+                [this.sensor.rayCount, 6, 4]
+            );
         }
-        this.sensor = new Sensor(this);
         this.controls = new Controls(controlType);
     }
 
@@ -27,6 +31,17 @@ class Car {
         }
         if (this.sensor) {
             this.sensor.update(roadBorders, traffic);
+            const offsets = this.sensor.readings.map(
+                s => s == null ? 0 : 1 - s.offset
+            );
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+            if (this.useBrain) {
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
         }
     }
 
@@ -36,7 +51,6 @@ class Car {
                 return true;
             }
         }
-
         for (let i = 0; i < traffic.length; i++) {
             if (polysIntersect(this.polygon, traffic[i].polygon)) {
                 return true;
@@ -61,7 +75,6 @@ class Car {
             x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
             y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad
         });
-
         points.push({
             x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
             y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad
@@ -76,12 +89,14 @@ class Car {
         if (this.controls.reverse) {
             this.speed -= this.acceleration;
         }
+
         if (this.speed > this.maxSpeed) {
-            this.speed = this.maxSpeed
+            this.speed = this.maxSpeed;
         }
         if (this.speed < -this.maxSpeed / 2) {
             this.speed = -this.maxSpeed / 2;
         }
+
         if (this.speed > 0) {
             this.speed -= this.friction;
         }
@@ -102,7 +117,6 @@ class Car {
             }
         }
 
-
         this.x -= Math.sin(this.angle) * this.speed;
         this.y -= Math.cos(this.angle) * this.speed;
     }
@@ -114,7 +128,7 @@ class Car {
             ctx.fillStyle = color;
         }
         ctx.beginPath();
-        ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
         for (let i = 1; i < this.polygon.length; i++) {
             ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
         }
